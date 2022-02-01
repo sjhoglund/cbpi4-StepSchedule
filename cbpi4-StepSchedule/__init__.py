f8780a5cd2c59d53b9e613ef67dac0320d2e06a2
Steven Hoglund <sjhoglund@gmail.com>
2022-01-31 21:56:25 -0800

    Test different plugin

--- cbpi4-StepSchedule/__init__.py
+++ cbpi4-StepSchedule/__init__.py
@@ -1,6 +1,4 @@
 
-# -*- coding: utf-8 -*-
-import os
 import asyncio
 import aiohttp
 from aiohttp import web
@@ -11,91 +9,94 @@ from datetime import datetime
 import time
 from cbpi.api import *
 import logging
-from unittest.mock import MagicMock, patch
 from socket import timeout
 from typing import KeysView
 from cbpi.api.config import ConfigType
 from cbpi.api.base import CBPiBase
 from voluptuous.schema_builder import message
 from cbpi.api.dataclasses import NotificationAction, NotificationType
+import numpy as np
 import requests
 import warnings
 
-logger = logging.getLogger(__name__)
 
-@parameters([
-     Property.Select(label="scheduleTime", options=[
-         "01",
-         "02",
-         "03",
-         "04",
-         "05",
-         "06",
-         "07",
-         "08",
-         "09",
-         "10",
-         "11",
-         "12",
-         "13",
-         "14",
-         "15",
-         "16",
-         "17",
-         "18",
-         "19",
-         "20",
-         "21",
-         "22",
-         "23",
-     ], description="Select a time for the brew steps to start running."),
-     Property.Select(label="AutoNext",options=["Yes","No"], description="Automatically move to next step (Yes) or pause after Notification (No)")
-])
+@parameters([Property.Number(label="Temp", configurable=True),
+             Property.Sensor(label="Sensor"),
+             Property.Kettle(label="Kettle"),
+             Property.Select(label="AutoMode",options=["Yes","No"], description="Switch Kettlelogic automatically on and off -> Yes")])
 
-class StepSchedule(CBPiStep):
-    
+class TEST_MashInStep(CBPiStep):
+
     async def NextStep(self, **kwargs):
         await self.next()
 
     async def on_timer_done(self,timer):
-        self.summary = 'Brew time!'
-
-        if self.AutoNext == True:
-            self.cbpi.notify(self.name, 'Moving to the next step.', NotificationType.INFO)
-            await self.next()
-        else:
-            self.cbpi.notify(self.name, 'Brew time. Hit the next step!', NotificationType.INFO, action=[NotificationAction("Next Step", self.NextStep)])
-            await self.push_update()
+        self.summary = "MashIn Temp reached. Please add Malt Pipe."
+        await self.push_update()
+        if self.AutoMode == True:
+            await self.setAutoMode(False)
+        self.cbpi.notify(self.name, 'MashIn Temp reached. Please add malt pipe and malt. Move to next step', action=[NotificationAction("Next Step", self.NextStep)])
 
     async def on_timer_update(self,timer, seconds):
         await self.push_update()
 
     async def on_start(self):
-        current_time = now.strftime("%H")
-        self.summary='Brew time scheduled for '+self.props.get("scheduleTime", "00")
-        self.AutoNext = False if self.props.get("AutoNext", "No") == "No" else True
+        self.port = str(self.cbpi.static_config.get('port',8000))
+        self.AutoMode = True if self.props.get("AutoMode","No") == "Yes" else False
+        self.kettle=self.get_kettle(self.props.get("Kettle", None))
+        self.kettle.target_temp = int(self.props.get("Temp", 0))
+        if self.AutoMode == True:
+            await self.setAutoMode(True)
+        self.summary = "Waiting for Target Temp"
         if self.timer is None:
             self.timer = Timer(1 ,on_update=self.on_timer_update, on_done=self.on_timer_done)
         await self.push_update()
 
     async def on_stop(self):
         await self.timer.stop()
-        self.summary = 'Not ready yet.'
+        self.summary = ""
+        if self.AutoMode == True:
+            await self.setAutoMode(False)
         await self.push_update()
 
     async def run(self):
         while self.running == True:
-            await asyncio.sleep(1)
-            current_time = now.strftime("%H")
-            if self.props.get("scheduleTime", "21") == current_time and self.timer.is_running is not True:
-                self.timer.start()
-                self.timer.is_running = True
+           await asyncio.sleep(1)
+           sensor_value = self.get_sensor_value(self.props.get("Sensor", None)).get("value")
+           if sensor_value >= int(self.props.get("Temp",0)) and self.timer.is_running is not True:
+               self.timer.start()
+               self.timer.is_running = True
         await self.push_update()
         return StepResult.DONE
-    
+
     async def reset(self):
         self.timer = Timer(1 ,on_update=self.on_timer_update, on_done=self.on_timer_done)
 
+    async def setAutoMode(self, auto_state):
+        try:
+            if (self.kettle.instance is None or self.kettle.instance.state == False) and (auto_state is True):
+                url="http://127.0.0.1:" + self.port + "/kettle/"+ self.kettle.id+"/toggle"
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(url) as response:
+                        return await response.text()
+                        await self.push_update()
+            elif (self.kettle.instance.state == True) and (auto_state is False):
+
+                await self.kettle.instance.stop()
+                await self.push_update()
+
+        except Exception as e:
+            logging.error("Failed to switch on KettleLogic {} {}".format(self.kettle.id, e))
+
+
 def setup(cbpi):
-    cbpi.plugin.register("StepSchedule", StepSchedule)
-    pass
+    '''
+    This method is called by the server during startup 
+    Here you need to register your plugins at the server
+
+    :param cbpi: the cbpi core 
+    :return: 
+    '''    
+    
+    cbpi.plugin.register("TEST_MashInStep", TEST_MashInStep)
+    
